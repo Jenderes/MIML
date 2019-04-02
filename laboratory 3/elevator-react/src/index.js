6f8ee80a1efb7d3d4f772618792311543bfabf02
Jenderes <frodlycin@gmail.com>
2019-04-02 22:36:06 +0300

    add 3 lab

---
+++ laboratory 3/elevator-react/src/index.js
@@ -0,0 +1,263 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+
+const maleIcon =
+	'<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 100 100" xml:space="preserve"><path d="M44.555,74.093c0,1.481,0.721,2.223,2.164,2.223c1.447,0,2.17-0.742,2.17-2.223V54.077h2.223v20.017  c0,1.481,0.725,2.223,2.17,2.223s2.17-0.742,2.17-2.223v-34.1h1.057v12.283c0,0.777,0.266,1.342,0.795,1.694  c0.527,0.353,1.072,0.353,1.641,0c0.563-0.352,0.844-0.917,0.844-1.694V39.04c0-1.412-0.479-2.505-1.428-3.282  c-0.951-0.777-2.273-1.167-3.967-1.167h-8.678c-1.623,0-2.943,0.354-3.965,1.06c-1.023,0.706-1.537,1.8-1.537,3.284v14.084  c0,0.634,0.283,1.093,0.848,1.375c0.564,0.283,1.111,0.283,1.639,0c0.527-0.282,0.795-0.741,0.795-1.375V39.994h1.061V74.093z"/><path d="M45.398,28.185c0,1.237,0.439,2.295,1.32,3.177c0.885,0.882,1.941,1.325,3.174,1.325c1.238,0,2.293-0.443,3.18-1.325  c0.879-0.881,1.32-1.959,1.32-3.231c0-1.198-0.441-2.241-1.32-3.122c-0.887-0.882-1.941-1.325-3.18-1.325  c-1.232,0-2.289,0.443-3.174,1.325C45.838,25.89,45.398,26.95,45.398,28.185z"/></svg>';
+
+const config = {
+	floors: 5,
+	elevators: 1,
+	minPeople: 1,
+	maxPeople: 6,
+	minInterval: 1000,
+	maxInterval:  2000
+};
+
+let data = {
+	floors: [],
+	elevators: []
+};
+let personId = 0;
+let arrobj = [];
+let inf = 0;
+let flag = true;
+let dlm = 0;
+let containerEl = document.querySelector(".container");
+function sleep() {
+	let i = 1000000000;
+	while (i > 0) {
+		i--;
+	}
+  };
+
+function randomExcept(min, max, except) {
+	let r = Math.floor(Math.random() * (max - min + 1) + min);
+	return r !== except ? r : randomExcept(min, max, except);
+}
+
+function getRandomPeople(min, max, currentFloorIndex, data) {
+	let lastArrival = 0;
+	return Array(Math.floor(Math.random() * (max - min + 1) + min))
+		.fill()
+		.map((item, index) => ({
+			index: personId++,
+			originalFloor: currentFloorIndex,
+			targetFloorIndex: randomExcept(0, config.floors, currentFloorIndex),
+			arrivesAt: (lastArrival +=
+				(Math.random() * config.maxInterval -
+					config.minInterval +
+					1 +
+					config.minInterval) |
+				0)
+        }));
+}
+for (let floorIndex = 0; floorIndex < config.floors; floorIndex++) {
+    let arr = [];
+    if (floorIndex === (config.floors - 1)) {
+        data.floors.push({
+            number: config.floors - floorIndex,
+            index: floorIndex,
+            callUp: false,
+            callDown: false,
+            people: arr
+        });
+    } else {
+      	data.floors.push({
+		number: config.floors - floorIndex,
+		index: floorIndex,
+		callUp: false,
+		callDown: false,
+        people: getRandomPeople(config.minPeople, config.maxPeople, floorIndex, data)  
+    });
+    }
+    dlm += data.floors[floorIndex].people.length;
+}
+async function demo(floorIndex) { 
+	if(floorIndex !== 4) {
+		for (let i = 1; i < arrobj.length; i++) {
+		if (arrobj[0].floor === arrobj[i].floor) {
+				arrobj.splice(i,1);
+				arrobj.splice(0,1);
+			break;
+		} else if (i === arrobj.length - 1) {
+			arrobj.splice(0,1);
+		}
+	}
+	await sleep(2000);
+	data.floors[floorIndex].people.shift();
+	data.floors[floorIndex].people.shift();
+	} 
+
+  };
+console.log("data", data);
+console.log("people", data.floors[0].people);
+for (let elevatorIndex = 0; elevatorIndex < config.elevators; elevatorIndex++) {
+	data.elevators.push({
+		index: elevatorIndex,
+		atFloor: (Math.random() * config.floors) | 0,
+		status: "open"
+	});
+}
+
+function callUp(floorIndex, data) {
+    return call(floorIndex, "up", data);
+}
+
+function callDown(floorIndex, data) {
+    return call(floorIndex, "down", data);
+}
+
+function call(floorIndex, direction, data) {
+	console.log("call", floorIndex, direction);
+	let newData = {
+        ...data,
+		elevators: [
+			{
+				...data.elevators[0],
+				atFloor: floorIndex
+			},
+			...data.elevators.slice(1)
+		]
+	};
+    console.log("arrobj",arrobj);
+	return tick(newData);
+}
+
+function renderFloor(floor, data) {
+	let doors = data.elevators.map((elevator, index) => (
+		<div
+			className={
+				"door door-" + index + (elevator.atFloor === floor.index ? " open" : "")
+			}
+			key={"door-" + index}
+		/>
+	));
+
+	let cls = ["floor", "floor-" + floor.index];
+	floor.callUp && cls.push("callUp");
+	floor.callDown && cls.push("callDown");
+
+	return (
+		<div className={cls.join(" ")} key={"floor-" + floor.index}>
+			{doors}
+			<div className="corridor" key="corridor">
+				<div className="number" key="numbers">
+					{floor.number}
+				</div>
+				<div className="buttons" key="buttons">
+					<button
+						className="button-up"
+						key="button-up"
+						onClick={() => callUp(floor.index, data)}
+					/>
+					<button
+						className="button-down"
+						key="button-down"
+						onClick={() => callDown(floor.index, data)}
+					/>
+				</div>
+				<div className="people" key="people">
+					{floor.people.map((person, index) => (
+						<div
+							className={"person" + (person.arrived ? " arrived" : "")}
+							data-target={person.targetFloorIndex}
+							key={"person-" + person.index}
+						>
+							<span dangerouslySetInnerHTML={{ __html: maleIcon }} />
+						</div>
+					))}
+				</div>
+			</div>
+		</div>
+	);
+}
+
+function renderShafts(data) {
+	return (
+		<div className="shafts">
+			{data.elevators.map((elevator, index) => (
+				<div className="shaft" key={"shaft-" + index}>
+					<div
+						className={[
+							"elevator",
+							"elevator-" + index,
+							"atFloor-" + elevator.atFloor
+						].join(" ")}
+						key={"elevator-" + elevator.index}
+					>
+						<div className={"door" + (elevator.status === "open" ? " open" : "")} />
+					</div>
+				</div>
+			))}
+		</div>
+	);
+}
+
+function tick(newData) {
+	data = newData;
+
+	ReactDOM.render(
+		<div className="building">
+			<div className="floors">
+				{data.floors.map((floor, index) => renderFloor(floor, data))}
+			</div>
+			{renderShafts(data)}
+		</div>,
+		document.querySelector(".container")
+	);
+}
+
+function init(data) {
+	data.floors.forEach((floor, index) => {
+		floor.people.forEach((person, index) => {
+			setTimeout(function() {
+                console.log("arrived", person);
+                arrobj[inf] = {floor:person.originalFloor,index:person.index };
+                inf+= 1;
+				person.arrived = true;
+				tick(data);	
+				inited(data);
+			}, person.arrivesAt);
+		});
+	});
+    tick(data);
+}
+function inited(data) {
+	if (inf === dlm) {
+	let t = 0;
+	while (t !== 10) {
+		if (arrobj.length !== 0) {
+			if(arrobj[0].floor !== 4) {
+				for (let i = 1; i < arrobj.length; i++) {
+				if (arrobj[0].floor === arrobj[i].floor) {
+						callDown(arrobj[0].floor,data);
+						sleep();
+						data.floors[arrobj[0].floor].people.shift();
+						data.floors[arrobj[0].floor].people.shift();
+						arrobj.splice(i,1);
+						arrobj.splice(0,1);
+						sleep();
+						callDown(4,data);
+					break;
+				} else if (i === arrobj.length - 1) {
+					callDown(arrobj[0].floor,data);
+					sleep();
+					data.floors[arrobj[0].floor].people.shift();
+					arrobj.splice(0,1);
+					callDown(4,data);
+				}
+			}
+			} 
+		sleep();
+		console.log(t);
+	}
+	t+=1;
+	}
+}
+}
+init(data);
+inited(data);
+console.log("arrobj",arrobj);
+serviceWorker.unregister();
